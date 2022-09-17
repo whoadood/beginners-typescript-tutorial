4ac312738cee9e2b21b1e66778f07d69cfc25fb8
Castle <castlerock88@protonmail.com>
2022-09-17 04:32:30 -0400

    beginner complete

--- src/14-extends.problem.ts
+++ src/14-extends.problem.ts
@@ -5,21 +5,20 @@ import { Equal, Expect } from "./helpers/type-utils";
  * interfaces. Can you find a way to refactor this to
  * make it more DRY?
  */
-
-interface User {
+interface Id {
   id: string;
+}
+interface User extends Id {
   firstName: string;
   lastName: string;
 }
 
-interface Post {
-  id: string;
+interface Post extends Id {
   title: string;
   body: string;
 }
 
-interface Comment {
-  id: string;
+interface Comment extends Id {
   comment: string;
 }
 
